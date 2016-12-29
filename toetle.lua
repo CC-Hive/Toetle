1fcfdf9419b90fb632882ccae4e00f987b38baec
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-12-29 22:17:36 +0000

    Create toetle.lua

---
+++ toetle.lua
@@ -0,0 +1,9 @@
+turtle = {} -- create dummy API
+turtle.toetle = {} -- insert our subAPI which will make the other one work
+
+
+
+
+
+
+return turtle -- just in case people want to load us this way
