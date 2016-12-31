4a8b1e5d22e7001132503c7c743dbcc4c12d220f
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-12-31 01:12:11 +0000

    making progress

--- toetle.lua
+++ toetle.lua
@@ -1,17 +1,50 @@
+if turtle then return end -- we're not needed so we'll just exit
+
 turtle = {} -- create dummy API
 turtle.toetle = {} -- insert our subAPI which will make the other one work
-turtle.toetle.lists = {} -- where we will store our responce lists
+turtle.toetle.lists = {} -- where we store our responces
+turtle.toetle.indexes = {}
+
+-- cache for quicker access
+local toetle = turtle.toetle
+local lists = toetle.lists
+local indexes = toetle.indexes
 
 -- add turtle functions
 -- TODO
 -- http://computercraft.info/wiki/Turtle_(API)
 
+function turtle.TEMPLATE()
+  if (not lists.TEMPLATE) or (not indexes.TEMPLATE) then
+    error("No responces for this function.",2)
+  else
+    local r = lists.TEMPLATE[indexes.TEMPLATE]
+    indexes.TEMPLATE = indexes.TEMPLATE + 1
+    if lists.TEMPLATE[indexes.TEMPLATE] == nil then
+      indexes.TEMPLATE = 1
+    end
+    return unpack(r)
+  end
+end
 
 
 -- add toetle functions
 -- TODO
 
-
+function toetle.TEMPLATE(...)
+  args = {...}
+  if not indexes.TEMPLATE then
+    indexes.TEMPLATE = 1
+  end
+  if not lists.TEMPLATE then
+    lists.TEMPLATE = {}
+  end
+  if not args[1] then
+    -- user wants nil
+    lists.TEMPLATE[indexes.TEMPLATE] = {}
+  end
+    
+end
 
 
 
